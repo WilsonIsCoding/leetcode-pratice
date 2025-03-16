06b57225f5879b8b7b986604e6446fdfce797aef
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-03-16 09:56:31 +0800

    feature：完全二元樹

--- easy/108. Convert Sorted Array to Binary Search Tree/code_0316.js
+++ easy/108. Convert Sorted Array to Binary Search Tree/code_0316.js
@@ -15,13 +15,37 @@
 // left = [1,2,3,4]
 // right = [6,7,8,9]
 var sortedArrayToBST = function (nums) {
-    if (nums.length === 0) return null
+  if (nums.length === 0) return null;
 
-    const mid = Math.floor(nums.length / 2);
-    const root = new TreeNode(nums[mid]);
+  const mid = Math.floor(nums.length / 2);
+  const root = new TreeNode(nums[mid]);
 
-    root.left = sortedArrayToBST(nums.slice(0, mid));
-    root.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
 
-    return root
+  return root;
+};
+
+// 「層序（level order）」建樹，類似「完全二元樹」的結構
+const arrayToBinaryTree = (arr) => {
+  if (arr.length === 0) return null;
+
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  let index = 1;
+
+  while (index < arr.length) {
+    const node = queue.shift();
+    if (index < arr.length) {
+      node.left = new TreeNode(arr[index]);
+      queue.push(node.left);
+      index++;
+    }
+    if (index < arr.length) {
+      node.right = new TreeNode(arr[index]);
+      queue.push(node.right);
+      index++;
+    }
+  }
+  return root;
 };
