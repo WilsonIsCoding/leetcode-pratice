e6032e5d506ad245d0028cebb0ac7b0c5f114556
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-06-21 14:52:57 +0800

    補上註解

--- medium/47. Permutations II/0419.js
+++ medium/47. Permutations II/0419.js
@@ -24,3 +24,27 @@ var permuteUnique = function (nums) {
     permutations([], numsSort)
     return result;
 };
+//set功能：紀錄已經使用過的數字，避免重複使用
+//input: [1,1,2]
+//output: [[1,1,2], [1,2,1], [2,1,1]]
+// 第一層 permutations([], [1,1,2])
+// for i=0: pickNum=1, used=[1], newRemaining=[1,2], path=[1], permutations([1], [1,2])
+// for i=1: pickNum=1, used.has(1)==true, continue
+// for i=2: pickNum=2, used=[1,2], newRemaining=[1,1], path=[2], permutations([2], [1,1])
+
+//第二層：permutations([1], [1,2])
+// for i=0: pickNum=1, used=[1], newRemaining=[2], path=[1,1], permutations([1,1], [2])
+// for i=1: pickNum=2, used=[1,2], newRemaining=[1], path=[1,2], permutations([1,2], [1])
+
+//第三層：permutations([1,1], [2])
+// for i=0: pickNum=2, used=[2], newRemaining=[1], path=[1,1,2], permutations([1,1,2], [])
+
+//第四層：permutations([1,2], [1])
+// for i=0: pickNum=1, used=[1], newRemaining=[2], path=[1,2,1], permutations([1,2,1], [])
+
+//第二層：permutations([2], [1,1])
+// for i=0: pickNum=1, used=[1], newRemaining=[1], path=[2,1], permutations([2,1], [1])
+// for i=1: pickNum=1, used.has(1)==true, continue
+
+//第三層：permutations([2,1], [1])
+// for i=0: pickNum=1, used=[1], newRemaining=[], path=[2,1,1], permutations([2,1,1], [])
