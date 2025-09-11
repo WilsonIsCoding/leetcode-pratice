a8c18ccb9987020a5c861a5b03cc7fa8620c9c5c
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-09-11 18:26:37 +0800

    add complexity

--- medium/2785. Sort Vowels in a String/0911.js
+++ medium/2785. Sort Vowels in a String/0911.js
@@ -4,7 +4,6 @@
  */
 var sortVowels = function (s) {
   let stack = [];
-  let position = [];
   s = s.split("");
   for (let i = 0; i < s.length; i++) {
     if (
@@ -15,16 +14,25 @@ var sortVowels = function (s) {
       s[i].toLowerCase() === "u"
     ) {
       stack.push(s[i]);
-      position.push(i);
     }
   }
   stack = stack.sort((a, b) => b.charCodeAt(0) - a.charCodeAt(0));
 
-  for (let i = 0; i < position.length; i++) {
-    let str = stack.pop();
-    s[position[i]] = str;
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s[i].toLowerCase() === "a" ||
+      s[i].toLowerCase() === "e" ||
+      s[i].toLowerCase() === "i" ||
+      s[i].toLowerCase() == "o" ||
+      s[i].toLowerCase() === "u"
+    ) {
+      let str = stack.pop();
+      s[i] = str;
+    }
   }
   return s.join("");
 };
 let s = "lEetcOde";
 console.log(sortVowels(s));
+// 複雜度：O(n+ NlogN +n)
+// 空間：O(2N)
