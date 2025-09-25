d6a037d80e5247f5d48e55d08faa4268e076892b
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-09-25 18:08:33 +0800

    0925 problem solve

---
+++ easy/1480. Running Sum of 1d Array/0925.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)):
+            nums[i]=nums[i]+nums[i-1]
+        return nums
+
+print(Solution().runningSum([1,2,3,4]))
+
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
