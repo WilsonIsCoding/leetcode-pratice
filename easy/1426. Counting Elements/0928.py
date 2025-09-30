f3eac3c79171752de30aeec051aeade648320976
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-09-30 09:34:56 +0800

    0930 problem solve

--- easy/1426. Counting Elements/0928.py
+++ easy/1426. Counting Elements/0928.py
@@ -8,6 +8,5 @@ class Solution:
         for i in range(len(arr)):
             if map.get(arr[i],0)>0 and map.get(arr[i]+1,0)>0:
                 count+=1
-                map[arr[i]]=map.get(arr[i])-1
         return map
 print(Solution().countElements([1,3,2,3,5,0]))
