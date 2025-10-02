2d788af32874813beeca4bf1c770dfe87eed6bba
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2025-10-02 09:41:59 +0800

    1002 problem solve

--- medium/567. Permutation in String/1002.py
+++ medium/567. Permutation in String/1002.py
@@ -6,19 +6,18 @@ class Solution:
         for str in s1:
             map[str] = map.get(str, 0) + 1
         left = 0
+        need = {}
         for right in range(len(s2)):
-            if s2[right] in map:
-                map[s2[right]] = map.get(s2[right]) - 1
-                if map.get(s2[right]) < 0:
-                    return False
-                if right - left == len(s1):
-                    return True
-            else:
-                while left != right:
-                    if map.get(s2[left]) is not None:
-                        map[s2[left]] = map.get(s2[left]) + 1
-                    left += 1
+            need[s2[right]] = need.get(s2[right], 0) + 1
+            if right - left + 1 > len(s1):
+                need[s2[left]] -= 1
+                if need[s2[left]] == 0:
+                    del need[s2[left]]
+                left += 1
+            if need == map:
+                return True
+
         return False
 
 
-print(Solution().checkInclusion("adc", "dcda"))
+print(Solution().checkInclusion("ab", "eidboaoo"))
